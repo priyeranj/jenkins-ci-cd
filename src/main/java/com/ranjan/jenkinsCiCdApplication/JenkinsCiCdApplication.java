8087d029dff2c04475cafeedc5990adc2282af5b
priyeranj <priyeranjan.moscow@gmail.com>
2024-06-22 20:38:48 +0530

    first jenkins demo

---
+++ src/main/java/com/ranjan/jenkinsCiCdApplication/JenkinsCiCdApplication.java
@@ -0,0 +1,21 @@
+package com.ranjan.jenkinsCiCdApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsCiCdApplication {
+
+	@GetMapping("greetings/{name}")
+	public String greetings(String name){
+		return "Hello " +name +" Congratulations you for successfully completed Jenkins CI/CD demo !";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCiCdApplication.class, args);
+	}
+
+}
